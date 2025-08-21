d253feb740a4b2837abbe94f91f7158e8ab7d2d6
Santan-Dave <tomasosicka32@gmail.com>
2025-08-21 16:10:50 +0200

    fix: conf

--- backend/production.py
+++ backend/production.py
@@ -9,7 +9,15 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'ZIQzSXEjTNyJuEzUrpBTBlhmhTAgVlso',
+        'HOST': 'postgres.railway.internal',
+        'PORT': '5432',
+    }
 }
+
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = "87vfyxwy5ov7*@1%*3gnrx0)2ck(e2a%f8e(f93e53#-wrb3w4"
