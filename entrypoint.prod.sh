8aee354a7fcf6b5db4fd2872709c73d93d3eef5b
Santan-Dave <tomasosicka32@gmail.com>
2025-08-21 15:32:13 +0200

    fix: production conf

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -1,3 +1,9 @@
+#!/bin/sh
+echo "Collecting static files..."
 python manage.py collectstatic --noinput
+
+echo "Applying database migrations..."
 python manage.py migrate --noinput
+
+echo "Starting Gunicorn..."
 python -m gunicorn --bind 0.0.0.0:1000 backend.wsgi:application
