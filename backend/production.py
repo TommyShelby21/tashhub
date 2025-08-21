e9fe9209f900d21cf1e1f0a871342065bfcbcaf4
Santan-Dave <tomasosicka32@gmail.com>
2025-08-21 15:50:27 +0200

    fix: conf

--- backend/production.py
+++ backend/production.py
@@ -1,5 +1,6 @@
 from .settings import *  # noqa: F403
 import dj_database_url
+import os
 
 DEBUG = False
 ALLOWED_HOSTS = ['*']
@@ -8,7 +9,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default="postgres://tomas:tomas@db:5432/taskhub"
-    )
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+SECRET_KEY = os.environ.get('SECRET_KEY')
